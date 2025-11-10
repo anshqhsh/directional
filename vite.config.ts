a723fb46ec4d17c4289860bd74628bb3a24add2f
JOON HYUK LEE <anshqhsh.dev@gmail.com>
2025-11-11 05:33:47 +0900

    chore: github 배포 수정

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "directional",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
